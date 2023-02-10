89cf16fb090a951af8107964932456ec6a9f3dda
Gian Ramirez <gece.ramirez@gmail.com>
2023-02-10 18:10:45 -0500

    add author component

---
+++ src/components/Author/Author.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export function Author() {
+  return (
+    <div className={styles.author}>
+      <a href="https://gramirez.me" target="_blank">
+        @zendostrike
+      </a>
+    </div>
+  );
+}
